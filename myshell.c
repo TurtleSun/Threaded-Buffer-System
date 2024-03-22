e7a0f16bff28af214c0c63fe46da3af9984b8880
Jake Gustin <gustinj@bu.edu>
2024-03-22 00:46:29 -0400

    added execvp call for testing, minor updates to reading input

--- myshell.c
+++ myshell.c
@@ -7,58 +7,58 @@
 #include <signal.h>
 
 void processCommands(char *);
-//void handleInterrupt();
+// void handleInterrupt();
 
-int main (int argc, char ** argv) {
+int main(int argc, char **argv)
+{
     int numPids = 0;
     int isTerminal = isatty(0);
-    char * cmds = malloc(1024);
-    int * pids = malloc(1024);
+    char *cmds = malloc(1024);
+    char *receivedInput = malloc(1024);
+    int *pids = malloc(1024);
 
-    if (cmds == NULL || pids == NULL) {
+    if (cmds == NULL || pids == NULL || receivedInput == NULL)
+    {
         fprintf(stderr, "malloc error.\n");
+        exit(1);
     }
-    //signal(SIGINT, handleInterrupt);
 
-    if (isTerminal) {
-        while(1) {
+    while (1)
+    {
+        if (isTerminal)
             printf("myshell> ");
-            char * termInput = fgets(cmds, 1024, stdin);
-            if (termInput == NULL) { // Ctrl-D issued.
-                printf("\n");
-                break;
-            }
-            printf("Got input from tty: %s", cmds);
-            processCommands(cmds);
-        }
-    } else {
-        while(1) {
-            char * fileInput = fgets(cmds, 1024, stdin);
-            if (fileInput == NULL || feof(stdin)) { // No more commands in the file
-                break;
-            }
-            printf("Got file input: %s", cmds);
-            processCommands(cmds);
-        }
-    }
 
+        fgets(receivedInput, 1024, stdin);
+        if (feof(stdin))
+            break; // Reached end of stdin, useful for file inputs
+        
+        printf("GOT INPUT: %s\n", receivedInput);
+        processCommands(cmds);
+    }
     return 0;
 }
 
-void processCommands(char * cmds) {
+void processCommands(char *cmds)
+{
     int forkRet = fork();
-    if (forkRet == -1) {
+    if (forkRet == -1)
+    { // Check if fork failed.
         exit(1);
-    } else if (forkRet == 0) {
-        printf("I AM CHILD - %d\n", forkRet);
-        exit(0);
-    } else {
-        printf("I AM PARENT - %d\n", forkRet);    
-        waitpid(forkRet, NULL, 0); 
     }
-    return;
+    else if (forkRet == 0)
+    {
+        printf("I AM CHILD - %d\n", forkRet); // Child proc
+        char *args[] = {NULL};                // Basic implementation of ls for testing
+        execvp("ls", args);
+        exit(0);
+    }
+    else
+    { // Parent proc.
+        printf("I AM PARENT - %d\n", forkRet);
+        waitpid(forkRet, NULL, 0);
+    }
 }
 
-//void handleInterrupt() { // Used to respond to SIGINT / Ctrl-c
-//    printf("handleInterrupt successful");
-//}
+// void handleInterrupt() { // Used to respond to SIGINT / Ctrl-c
+//     printf("handleInterrupt successful");
+// }
