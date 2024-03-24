c6e7f32ecac617f586fae63dbae430c1b8886353
Jake Gustin <gustinj@bu.edu>
2024-03-24 00:01:17 -0400

    fixing bugs with redirection

--- TODO.md
+++ TODO.md
@@ -1,15 +1,4 @@
 ## Myshell
-- Enable output redirection in myshell
-  - `ls > stdout.txt`
-    - redirect stdout to file
-  - `ls 1> stdout.txt`
-    - redirect stdout to file (same as above)
-  - `ls 2> stderr.txt`
-    - redirect stderr to file
-  - `ls &> stdout_stderr.txt` 
-    - redirect stdout and stderr to same file
-  - `ls < in.txt`
-    - redirect file to stdin
 - Enable piping in myshell
   - `ls | cat`
   - `ls -la | cat`
--- myshell.c
+++ myshell.c
@@ -4,24 +4,66 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <signal.h>
+#include <fcntl.h>
 
+// Used to maintain list of commands.
 int numCmds;
 char **inputtedCmds;
 
+// Used to maintain list of tokens in a command.
+int numTokens;
+char **tokens;
+
+// Used to maintain list of child processes (especially background tasks)
 int numPids;
 int *pids;
 
+// receivedInput acts as input buffer. Compared against prevInput to check for EOF.
 char *receivedInput;
-char * prevInput; // used for detecting EOF.
+char *prevInput;
+
+// Used to maintain file descriptors for files used in redirections.
+struct openedFile
+{
+    char *filename;
+    int fd;
+};
+struct openedFile openedFiles[128];
+int currentFileIndex;
+
+
+// Used for resetting file descriptors after redirections.
+int savedStdout;
+int savedStdin;
+int savedStderr;
 
 void processInput(char *);
 void executeCommands(char **);
 char **splitBySpace(char *);
+void setRedirections(char **);
+void resetRedirections(void);
+int openFile(char *, int);
+void linkFileDescriptors(int, int);
+int isFileOpened(char *);
 
 int main(int argc, char **argv)
 {
+    // Save the current file descriptors for stdout, stdin, and stderr.
+    savedStdout = dup(1);
+    savedStdin = dup(0);
+    savedStderr = dup(2);
+
+    if (savedStdout == -1 || savedStdin == -1 || savedStderr == -1)
+    {
+        perror("dup error");
+        exit(1);
+    }
+
     numPids = 0;
+    currentFileIndex = 0;
+
     int isTerminal = isatty(0);
 
     // Initialize memory, assuming one element for now.
@@ -33,7 +75,7 @@ int main(int argc, char **argv)
     // Check if malloc was successful.
     if (pids == NULL || receivedInput == NULL || prevInput == NULL || inputtedCmds == NULL)
     {
-        fprintf(stderr, "malloc error.\n");
+        perror("malloc error");
         exit(1);
     }
 
@@ -56,18 +98,19 @@ int main(int argc, char **argv)
         strcpy(prevInput, receivedInput);
 
         // Begin processing.
-        printf("GOT INPUT: %s\n", receivedInput);
         processInput(receivedInput);
     }
     return 0;
 }
 
+// Processing string input to determine commands to run
 void processInput(char *str)
 {
-    char * strCopy = malloc(strlen(str) + 1);
+    // Copy input to a new string to avoid modifying the original.
+    char *strCopy = malloc(strlen(str) + 1);
     if (strCopy == NULL)
     {
-        fprintf(stderr, "malloc error.\n");
+        perror("malloc error");
         exit(1);
     }
     strcpy(strCopy, str);
@@ -79,7 +122,7 @@ void processInput(char *str)
     numCmds = 0;
     while (1)
     {
-         // Check if there are no more commands
+        // Check if there are no more commands
         if (strcmp(strCopy, "") == 0)
             break;
 
@@ -91,7 +134,7 @@ void processInput(char *str)
         inputtedCmds = realloc(inputtedCmds, sizeof(char *) * (numCmds + 1));
         if (inputtedCmds == NULL || inputtedCmds[numCmds] == NULL)
         {
-            fprintf(stderr, "malloc error.\n");
+            perror("malloc error");
             exit(1);
         }
 
@@ -107,7 +150,7 @@ void processInput(char *str)
     for (int i = 0; i < numCmds; i++)
     {
         // Extract the command and arguments by means of checking the spaces.
-        char **tokens = splitBySpace(inputtedCmds[i]);
+        tokens = splitBySpace(inputtedCmds[i]);
         executeCommands(tokens);
     }
 }
@@ -116,15 +159,16 @@ void processInput(char *str)
 char **splitBySpace(char *str)
 {
     // Initializing memory, assuming one element for now in tokens.
-    char **tokens = malloc(sizeof(char *));
+    tokens = malloc(sizeof(char *));
     if (tokens == NULL)
     {
-        fprintf(stderr, "malloc error.\n");
+        perror("malloc error");
         exit(1);
     }
 
-    int numTokens = 0;
-    while (1) {
+    numTokens = 0;
+    while (1)
+    {
         // Check if there are no more commands
         if (strcmp(str, "") == 0)
             break;
@@ -136,12 +180,12 @@ char **splitBySpace(char *str)
         if (str == NULL)
             break;
 
-        // Allocating memory    
-        tokens[numTokens] = malloc(strlen(str) + 1);
+        // Allocating memory
         tokens = realloc(tokens, sizeof(char *) * (numTokens + 1));
+        tokens[numTokens] = malloc(strlen(str) + 1);
         if (tokens == NULL || tokens[numTokens] == NULL)
         {
-            fprintf(stderr, "malloc error.\n");
+            perror("malloc error");
             exit(1);
         }
 
@@ -155,23 +199,161 @@ char **splitBySpace(char *str)
 }
 
 // Executes the commands in the tokens array.
-void executeCommands(char ** tokens) {
+void executeCommands(char **tokens)
+{
+    setRedirections(tokens);
+
     // Fork and execute the command.
     int forkRet = fork();
     if (forkRet == -1) // Fork failed
     {
-        fprintf(stderr, "fork error.\n");
+        perror("fork error");
         exit(1);
     }
     else if (forkRet == 0) // Child process - do the command.
     {
-        printf("I AM CHILD - %d\n", forkRet); // Child proc
         execvp(tokens[0], tokens);
         exit(0);
     }
     else // Parent process - just wait for the child.
     {
-        printf("I AM PARENT - %d\n", forkRet);
         waitpid(forkRet, NULL, 0);
     }
+
+    resetRedirections();
+}
+
+// Iterates over the list of tokens for a command and sets any needed redirections, in and out.
+void setRedirections(char **tokens)
+{
+    int isRedirecting = 0;
+    int newfd = -2;
+
+    for (int i = 0; i < numTokens; i++)
+    {
+        isRedirecting = 0;
+
+        // Redirect stdout and stderr to the same file
+        if (strcmp(tokens[i], "&>") == 0)
+        {
+            int fileFD = isFileOpened(tokens[i+1]);
+            if (fileFD == -1)
+                newfd = openFile(tokens[i + 1], O_WRONLY | O_CREAT | O_TRUNC);
+            else
+                newfd = fileFD;
+
+            linkFileDescriptors(1, newfd);
+            linkFileDescriptors(2, newfd);
+            isRedirecting = 1;
+        }
+
+        // Redirecting stdout to a file.
+        if (strcmp(tokens[i], ">") == 0 || strcmp(tokens[i], "1>") == 0)
+        {
+            int fileFD = isFileOpened(tokens[i+1]);
+            if (fileFD == -1)
+                newfd = openFile(tokens[i + 1], O_WRONLY | O_CREAT | O_TRUNC);
+            else
+                newfd = fileFD;
+
+            linkFileDescriptors(1, newfd);
+            isRedirecting = 1;
+        }
+
+        // Redirecting stderr to a file.
+        if (strcmp(tokens[i], "2>") == 0)
+        {
+            int fileFD = isFileOpened(tokens[i+1]);
+            if (fileFD == -1)
+                newfd = openFile(tokens[i + 1], O_WRONLY | O_CREAT | O_TRUNC);
+            else
+                newfd = fileFD;
+
+            linkFileDescriptors(2, newfd);
+            isRedirecting = 1;
+        }
+
+        // Redirecting stdin to a file.
+        if (strcmp(tokens[i], "<") == 0)
+        {
+            int fileFD = isFileOpened(tokens[i+1]);
+
+            if (fileFD == -1)
+                newfd = openFile(tokens[i + 1], O_RDONLY);
+            else
+                newfd = fileFD;
+
+            linkFileDescriptors(0, newfd);
+            isRedirecting = 1;
+        }
+
+        // We did some redirection on the current token. Adjust the tokens array accordingly for later execution.
+        if (isRedirecting)
+        {
+            // Remove the redirection operator and the target file from the tokens array.
+            for (int j = i; j < numTokens - 2; j++)
+                tokens[j] = tokens[j + 2];
+
+            // After shifting, set the last two elements to NULL.
+            tokens[numTokens - 2] = NULL;
+            tokens[numTokens - 1] = NULL;
+
+            // Adjust the number of tokens, and decrement i to check the current, shifted token again.
+            numTokens -= 2;
+            i--;
+        }
+    }
+}
+
+// Used after executing a command to reset the file descriptors to their original state.
+void resetRedirections()
+{
+    int resetStdout = dup2(savedStdout, 1);
+    int resetStdin = dup2(savedStdin, 0);
+    int resetStderr = dup2(savedStderr, 2);
+    if (resetStdout == -1 || resetStdin == -1 || resetStderr == -1)
+    {
+        perror("dup error");
+        exit(1);
+    }
+}
+
+// Utilizes dup2 to link file descriptors to the target file.
+void linkFileDescriptors(int oldfd, int newfd)
+{
+    // Duplicate target file stream to stdin.
+    int dupRet = dup2(newfd, oldfd);
+    if (dupRet == -1)
+    {
+        perror("dup error");
+        exit(1);
+    }
+}
+
+// Opens a file with the given flags and returns the file descriptor.
+int openFile(char *file, int flags)
+{
+    // Open the target file.
+    int newfd = open(file, flags , 0644);
+    if (newfd == -1)
+    {
+        perror("open error");
+        exit(1);
+    }
+
+    openedFiles[currentFileIndex].filename = file;
+    openedFiles[currentFileIndex].fd = newfd;
+    currentFileIndex++;
+
+    return newfd;
+}
+
+// Checks if we've previously openend a file. If so, return the file descriptor.
+int isFileOpened(char * filename) {
+    for (int i = 0; i < 128; i++) {
+        if (openedFiles[i].filename != NULL && strcmp(openedFiles[i].filename, filename) == 0) {
+            return openedFiles[i].fd;
+        }
+    }
+    return -1;
 }
