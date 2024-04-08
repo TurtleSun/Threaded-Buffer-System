8addc74be378ced09168552e3347adb7efafe643
paulitab <plopezburgos@gmail.com>
2024-04-08 18:51:58 -0400

    updated observe to initialize the buffer sttruct properly and call writeBuffer

--- observe.c
+++ observe.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <sys/shm.h>
 #include <unistd.h>
-#include <buffer.h>
+#include <bufferLib.h>
 
 //  reads input either from a file or stdin. The input data will be a series of strings, each terminated by a newline either until the end-of-file input or the user types ctrl-d to end the standard input stream. NOTE that ctrl-d (ctrl  and the 'd' key together) sends an EOF signal to the foreground process, so as long as tapper is running, it will not terminate your shell program. 
 // Each string input to P1 will be of the form:
@@ -16,17 +16,53 @@
 #define MAX_VALUE_LEN 100
 #define MAX_LINE_LEN 200
 #define KEY 1234
+            /// should this be buffer_size?
 #define SHMSIZE 100000
 
 int main(){
     // open shared memory that we initialized in tapper
-    int shm_fd = shmget(KEY, SHMSIZE, 0666);
-    void * shm_addr = shmat(shm_fd, NULL, 0);
+    int shm_Id = shmget(KEY, SHMSIZE, 0666);
+    if (shm_Id == -1) {
+    // something went horribly wrong        
+    perror("shmget error");
+    exit(1);
+    }
+
+    void * shm_addr = shmat(shm_Id, NULL, 0);
     if (shm_addr == NULL) {
         // something still went horribly wrong
         perror("malloc error");
         exit(1);
     }
+
+    // cast shared memory to a buffer
+    Buffer *shmBuffer = (Buffer *)shm_addr;
+
+    // intialize buffer
+    // depending on the buffer type, we need to initialize the buffer differently
+    // check from inputs
+    // Variables for command line arguments
+    char *bufferType = NULL;
+    int bufferSize = 0;
+
+    // Parsing command line arguments
+    int opt;
+    while ((opt = getopt(argc, argv, "b:s:")) != -1) {
+        switch (opt) {
+            case 'b':
+                bufferType = optarg;
+                break;
+            case 's':
+                bufferSize = atoi(optarg);
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-b bufferType] [-s bufferSize]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    // Initialize the buffer with received type and size
+    Buffer buffer = initBuffer(bufferType, bufferSize);
 
     // read and parse from either a file or stdin
     char line[MAX_LINE_LEN];
@@ -52,7 +88,14 @@ int main(){
             strcpy(lastValue, value);
             // Write to shared memory
             //TODO: Change shm_addr here to an attribute (slot) of a structure that we cast shm_addr to.
-            snprintf(shm_addr, MAX_LINE_LEN, "%s=%s", name, value);
+            // snprintf(shm_addr, MAX_LINE_LEN, "%s=%s", name, value);
+
+            // Prepare the string to write into the buffer
+            char bufferData[MAX_LINE_LEN];
+            snprintf(bufferData, sizeof(bufferData), "%s=%s", name, value);
+            
+            // Write into the buffer based on its type
+            writeBuffer(&buffer, bufferData);
         }
     }
 
